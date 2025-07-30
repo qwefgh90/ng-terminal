fec5fa484c81d57bebfcbe3568842999c781da4e
Choe Changwon <qwefgh90@naver.com>
2025-07-30 21:56:34 +0900

    update material to 15

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
+import { MatLegacySlideToggleModule as MatSlideToggleModule } from '@angular/material/legacy-slide-toggle';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
+import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
 import { NgTerminalModule } from 'ng-terminal';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
+import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
 import { NgxJsonViewerModule } from 'ngx-json-viewer';
 import { ExampleComponent } from './example.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { MatTabsModule } from '@angular/material/tabs';
+import {MatLegacyFormFieldModule as MatFormFieldModule} from '@angular/material/legacy-form-field';
+import { MatLegacyTabsModule as MatTabsModule } from '@angular/material/legacy-tabs';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 @NgModule({
   declarations: [
